9fcca4817165324f45a3a76f46bcc32a6168af37
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-02-14 16:37:22 +0700

    init + ide

---
+++ src/modules/ControlledThread.java
@@ -0,0 +1,51 @@
+package modules;
+
+public abstract class ControlledThread implements Runnable {
+	private Thread t;
+	private String threadName;
+	private volatile boolean running = true;
+	private volatile boolean paused = false;
+
+	public ControlledThread(String threadName) {
+		super();
+		this.threadName = threadName;
+		start();
+	}
+
+	public void run() {
+		while (running) {
+			synchronized (this) {
+				while (paused) {
+					try {
+						wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				update();
+			}
+		}
+	}
+	
+	public abstract void update();
+	
+	public void start() {
+		if (t == null) {
+			t = new Thread(this, this.threadName);
+			t.start();
+		}
+	}
+
+	public void pause() {
+		paused = true;
+	}
+
+	public synchronized void resume() {
+		paused = false;
+		notify();
+	}
+
+	public void stop() {
+		running = false;
+	}
+}
