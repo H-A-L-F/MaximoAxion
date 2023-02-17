49ee116aba5e402385524f29ed266675334cd4d3
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-02-17 10:39:11 +0700

    initGame

--- src/main/GameMaster.java
+++ src/main/GameMaster.java
@@ -2,11 +2,14 @@ package main;
 
 import models.player.Player;
 import models.user.User;
+import models.world.World;
 import modules.Database;
 
 public class GameMaster {
-	public User user;
+	public World world;
 	public Player player;
+	
+	public User user;
 	private Database db = Database.getInstance();
 	
 	public GameMaster(User user) {
@@ -14,7 +17,8 @@ public class GameMaster {
 	}
 	
 	public void initGame() {
-		
+		this.world = new World();
+		this.player = new Player();
 	}
 	
 	public void newGame() {
