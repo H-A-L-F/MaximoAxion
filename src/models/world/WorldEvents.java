458cb528d5e40993413c7001ae6dd067bb408f12
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-02-19 16:18:05 +0700

    worldEvents now has message

--- src/models/world/WorldEvents.java
+++ src/models/world/WorldEvents.java
@@ -1,16 +1,28 @@
 package models.world;
 
 public enum WorldEvents {
-	VOLCANIC_ERUPTION,
-	MASSIVE_THUNDERSTORM,
-	MASSIVE_BIRD_MIGRATION,
-	MASSIVE_EARTHQUAKE,
+	WORLD_NORMAL("The world is peaceful and everything seems to be in order."),
+
+	VOLCANIC_ERUPTION("The ground is shaking and the sky is darkened by ash - a volcano has erupted!"),
+	MASSIVE_THUNDERSTORM("The sky is dark and lightning is striking everywhere - a massive thunderstorm is raging!"),
+	MASSIVE_BIRD_MIGRATION("The sky is filled with birds of all kinds, moving in a massive migration."),
+	MASSIVE_EARTHQUAKE("The ground is shaking violently and buildings are collapsing - a massive earthquake is underway!"),
+
+	SOLAR_ECLIPSE("The sun is obscured by the moon, casting an eerie shadow on the world - it's a solar eclipse!"),
+	SCORCHING_SUN("The sun is burning hot and the air is stifling - it's a scorching day!"),
+
+	BLOOD_MOON("The moon is red and the sky is eerie - it's a blood moon!"),
+	MASSIVE_BATS_INVASION("The sky is filled with swarms of bats, covering the world in darkness."),
+	ALIEN_INVASION("Strange objects are falling from the sky and people are running in fear - it's an alien invasion!"),
+	BLACKOUT("The world is dark and silent, as a massive blackout has plunged everything into darkness.");
+		
+	private final String message;
 	
-	SOLAR_ECLIPSE,
-	SCORCHING_SUN,
+	WorldEvents(String message) {
+		this.message = message;
+	}
 	
-	BLOOD_MOON,
-	MASSIVE_BATS_INVASION,
-	ALIEN_INVASION,
-	BLACKOUT;
+	public String getMessage() {
+		return this.message;
+	}
 }
