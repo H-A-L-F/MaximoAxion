be69b75232743e0368e911b4619734813ac15f9b
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-02-20 14:45:17 +0700

    complete player actions

--- src/models/player/PlayerActions.java
+++ src/models/player/PlayerActions.java
@@ -1,5 +1,12 @@
 package models.player;
 
+import java.util.AbstractMap;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Vector;
+
+import models.world.WorldEvents;
+
 public enum PlayerActions {
 	EXPLORE,
 	REST,
@@ -19,5 +26,49 @@ public enum PlayerActions {
 	
 	ATTACK,
 	DEFEND,
-	FLEE
+	FLEE;
+	
+	public static Vector<PlayerActions> getFreeActions() {
+		return new Vector<>(Arrays.asList(EXPLORE, REST));
+	}
+	
+	public static Vector<PlayerActions> getWoodActions() {
+		return new Vector<>(Arrays.asList(WOOD_GATHER, BUILD_SHELTER, IMPROVE_SHELTER));
+	}
+	
+	public static Vector<PlayerActions> getFoodActions() {
+		return new Vector<>(Arrays.asList(FOOD_GATHER, FOOD_CONSUME));
+	}
+	
+	public static Vector<PlayerActions> getWaterActions() {
+		return new Vector<>(Arrays.asList(WATER_GATHER, WATER_CONSUME));
+	}
+	
+	public static Vector<PlayerActions> getHerbsActions() {
+		return new Vector<>(Arrays.asList(HERBS_GATHER, HERBS_CONSUME));
+	}
+	
+	public static Vector<PlayerActions> getEncounterActions() {
+		return new Vector<>(Arrays.asList(ATTACK, DEFEND, FLEE));
+	}
+	
+	public static Map<ActionTypes, Vector<PlayerActions>> getActions() {
+		return Map.ofEntries(
+				  new AbstractMap.SimpleEntry<ActionTypes, Vector<PlayerActions>>(ActionTypes.FREE, getFreeActions()),
+				  new AbstractMap.SimpleEntry<ActionTypes, Vector<PlayerActions>>(ActionTypes.WOOD, getWoodActions()),
+				  new AbstractMap.SimpleEntry<ActionTypes, Vector<PlayerActions>>(ActionTypes.FOOD, getFoodActions()),
+				  new AbstractMap.SimpleEntry<ActionTypes, Vector<PlayerActions>>(ActionTypes.WATER, getWaterActions()),
+				  new AbstractMap.SimpleEntry<ActionTypes, Vector<PlayerActions>>(ActionTypes.HERBS, getHerbsActions()),
+				  new AbstractMap.SimpleEntry<ActionTypes, Vector<PlayerActions>>(ActionTypes.ENCOUNTER, getEncounterActions())
+				);
+	}
+	
+	enum ActionTypes {
+		FREE,
+		WOOD,
+		FOOD,
+		WATER,
+		HERBS,
+		ENCOUNTER
+	}
 }
