f95468b85aac3289b320acce1a2851d7eb49e428
Arya <64455337+H-A-L-F@users.noreply.github.com>
2023-03-02 15:16:58 +0700

    tidying

--- src/main/GameInput.java
+++ src/main/GameInput.java
@@ -34,20 +34,6 @@ public class GameInput extends ControlledThread {
 
 	@Override
 	public void update() {
-//		String message = String.format("0 to pause\nWhat do you want to do? [1..%d]", gm.player.availActions.size());
-////		String res = in.getStrWMSG(message, 1, gm.player.availActions.size());
-//		// TODO HARDCODE FIX
-//		String res = in.getStrWMSG("", 1, gm.player.availActions.size());
-//
-//		try {
-//			int p = Integer.parseInt(res);
-//			if(p >= gm.player.availActions.size()) return;
-//		} catch (Exception e) {
-//			// TODO: handle exception
-//		}
-//		
-//		gm.input(res);
-
 		String res = in.getStrWMSG("", 1, gm.player.availActions.size());
 
 		if (!running) return;
